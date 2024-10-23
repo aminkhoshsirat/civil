7d3198a47f4bb2cca029e61d8b0c8558d54349de
kourosh-nik <kurosh1983@gmail.com>
2024-10-23 12:23:49 +0330

    init

---
+++ shop/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def store(request):
+    return render(request,"store.html")
+
+def checkout(request):
+    return render(request,"checkout.html")
+
+def product(request):
+    return render(request,"product.html")
