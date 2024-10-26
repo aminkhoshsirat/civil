ca7bea45f3eff18a90acba9759a5f3df88bc0ee8
kourosh-nik <kurosh1983@gmail.com>
2024-10-26 20:02:38 +0330

    Add accounts app

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+# admin.site.register(models.Cart)
+# admin.site.register(models.Order)
+# admin.site.register(models.Category)
+# admin.site.register(models.Product)
+# admin.site.register(models.OrderProduct)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
+    search_fields = ['title']
+
+    def delete_queryset(self, request, queryset):
+        for category in queryset:
+            category.delete()
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','title','price','quantity','status']
+    search_fields = ['title']
+    list_filter = ['status']
+
+    def delete_queryset(self, request, queryset):
+        for product in queryset:
+            product.delete()
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id','product','user','quantity']
+
+    def delete_queryset(self, request, queryset):
+        for cart in queryset:
+            cart.delete()
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','user','total_price','status']
+    list_filter = ['status']
+
+    def delete_queryset(self, request, queryset):
+        for order in queryset:
+            order.delete()
+
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ['id','order_id','product','quantity', 'price']
+
+    def delete_queryset(self, request, queryset):
+        for order in queryset:
+            order.delete()
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Cart, CartAdmin)
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderProduct, OrderProductAdmin)
--- shop_one/settings.py
+++ shop_one/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'website',
     'shop',
+    'accounts',
 ]
 
 MIDDLEWARE = [
