88fb94ba2c3c468f100d35ff44e02e1373300aed
kourosh-nik <kurosh1983@gmail.com>
2024-11-27 12:03:56 +0330

    Renamr mainFolder to structure

--- structure/asgi.py
+++ structure/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for mainFolder project.
+ASGI config for structure project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainFolder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'structure.settings')
 
 application = get_asgi_application()
